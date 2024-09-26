9498e0b5d3ac44d5882f73efb5171d60a7636a90
Gasca <lmgasca@cps.k12.il.us>
2024-09-26 08:30:39 -0500

    first commit

---
+++ Week5_day_2.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "orange", "banana", "coconut", "pineapple"]
+print(dir(fruits))
+print(help(fruits))
+print(len(fruits))
+print("apple" in fruits)
+
+#fruits[0] = "pineapple" # re-assign values using this
+#fruits.append("pineapple")
+#fruits.remove("apple")
+#fruits.insert(0, "pineapple")
+#fruits.sort()
+#fruits.reverse()
+
+# print(fruits[0])
+
+for fruit in fruits:
+    print(fruit)
